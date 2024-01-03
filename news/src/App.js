a62db1d6697da2cf01f42d97527d59c1992a223a
olusolabobby <olusolaadelabu89@gmail.com>
2024-01-02 21:40:55 -0800

    second init

--- news/src/App.js
+++ news/src/App.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const alanKey = 'a5a3ee1740e2b08a73941d06d3eedebe2e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+
+    const [newsArticles, setNewsArticles] = useState([]);
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
             onCommand: ({ command, articles }) => {
                 if(command === 'newHeadlines') {
-                    console.log(articles);
+                    setNewsArticles(articles);
                 }
             }
         })
@@ -18,6 +21,7 @@ const App = () => {
     return (
         <div>
             <h1>AI News Application</h1>
+            <NewsCards articles ={newsArticles} />
         </div>
     )
 }
