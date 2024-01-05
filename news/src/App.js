e05d5cd44b279070debf56ded6b51cf6d2f09aa5
olusolabobby <olusolaadelabu89@gmail.com>
2024-01-05 14:38:02 -0800

    Fifth init

--- news/src/App.js
+++ news/src/App.js
@@ -22,18 +22,19 @@ const App = () => {
                 } else if (command === 'highlight') {
                     setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
                 } else if (command === 'open') {
-                    // for will be four
+                    // for words will be four in words to numbers
                     const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
                     const article = articles[parsedNumber -1];
 
                     if(parsedNumber > 20) {
-                        alanBtn().playText('Opening.')
+                        alanBtn().playText('Please try again.')
+                    } else if (article) {
+                        window.open(article.url, '_blank');
+                        // alanBtn().playText('Opening ... ');
+                    } else {
+                        // alanBtn().playText('Opening ... ');
+                        window.open(articles[number].url, '_blank');
                     }
-                    // else if (article) {
-                    //     window.open(article.url, '_blank');
-                    //     alanBtn().playText('Opening right away. ')
-                    // }
-                    window.open(articles[number].url, '_blank');
                 }
             }
         })
