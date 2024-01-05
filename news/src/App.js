00078ce363fcffb64a0bd80f875a1cccbca6e301
olusolabobby <olusolaadelabu89@gmail.com>
2024-01-05 10:14:03 -0800

    Fourth init

--- news/src/App.js
+++ news/src/App.js
@@ -1,18 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from "words-to-numbers";
 import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from './styles.js';
+
 
 const alanKey = 'a5a3ee1740e2b08a73941d06d3eedebe2e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
 
     const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command, articles }) => {
+            onCommand: ({ command, articles, number }) => {
                 if(command === 'newHeadlines') {
                     setNewsArticles(articles);
+                    setActiveArticle(-1)
+                } else if (command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                } else if (command === 'open') {
+                    // for will be four
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+                    const article = articles[parsedNumber -1];
+
+                    if(parsedNumber > 20) {
+                        alanBtn().playText('Opening.')
+                    }
+                    // else if (article) {
+                    //     window.open(article.url, '_blank');
+                    //     alanBtn().playText('Opening right away. ')
+                    // }
+                    window.open(articles[number].url, '_blank');
                 }
             }
         })
@@ -20,8 +41,11 @@ const App = () => {
 
     return (
         <div>
-            <h1>AI News Application</h1>
-            <NewsCards articles ={newsArticles} />
+            <div className={classes.logoContainer}>
+                <img src="https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/vraagoyqfwbbicfuxujj" alt="AI Logo" className={classes.alanLogo} />
+            </div>
+            <h1 className={classes.voice}>AI Voice Assistance</h1>
+            <NewsCards articles ={newsArticles} activeArticle={activeArticle}/>
         </div>
     )
 }
--- news/src/components/NewsCards/NewsCards.js
+++ news/src/components/NewsCards/NewsCards.js
@@ -3,15 +3,60 @@ import NewsCard from '../NewsCard/NewsCard';
 import { Grid, Grow, Typography } from '@material-ui/core';
 import useStyles from './styles.js';
 
-const NewsCards = ({ articles }) => {
+const infoCards = [
+    { color: '#121212', title: 'Recently posted News', info: 'General, Sports, Fashion', text: 'Show me the recent news' },
+    { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Sports, Technology', text: 'Give me the latest Technology news' },
+    { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, Play station 5, Smartphones', text: 'what\'s up with Play Station 5 '},
+    { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time', text: 'Give me the news from CNN ' },
+];
+
+
+const NewsCards = ({ articles, activeArticle }) => {
     const classes = useStyles(); // useStyle is used as a hook to define classes
+
+    if(!articles.length){
+        return (
+            <Grow in>
+                <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                    {infoCards.map((infoCard) => (
+                        <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
+                            <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
+
+                                <Typography variant="h5">{infoCard.title}</Typography>
+                                {
+                                    infoCard.info
+                                        ? (<Typography variant="h6">
+                                            <strong>
+                                                {infoCard.title.split (' ')[2]}:
+                                                <br /><br />
+                                            </strong>
+
+                                            {infoCard.info}
+                                        </Typography>)
+                                        :
+                                        null
+                                }
+                                <br />
+                                <Typography variant="h6">Attempt by saying: <br />
+                                    <i>
+                                        {infoCard.text}
+                                    </i>
+                                </Typography>
+
+                            </div>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grow>
+        );
+    }
 
     return (
         <Grow in>
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {articles.map((article, i) => (
                     <Grid item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}>
-                        <NewsCard article={article} i={i} />
+                        <NewsCard article={article} activeArticle={activeArticle} i={i} />
                     </Grid>
                 ))}
             </Grid>
